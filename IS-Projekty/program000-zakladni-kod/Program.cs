3edbc858ec3b63fa2e227c428ba6f6910a579a4d
Tomáš Žižka <tomas.zizka@tul.cz>
2024-10-09 09:44:47 +0200

    #1 zakladní kód s opakovanim programu

---
+++ IS-Projekty/program000-zakladni-kod/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Program {
+    static void Main() {
+        
+        string again = "a";
+        
+        while(again == "a") {
+            Console.Clear();
+            Console.WriteLine("****************************");
+            Console.WriteLine("***** Výpis řady čísel *****");
+            Console.WriteLine("****************************");
+            Console.WriteLine("******* Tomáš Žižka ********");
+            Console.WriteLine("****************************");
+            Console.WriteLine();
+
+
+
+            Console.WriteLine();
+            Console.WriteLine("Pro opakování programu stiskněte klávesu A");
+            again = Console.ReadLine();
+
+        }
+
+    }
+}    
+
+
+    
+
+
+
+
+// toto je jednořádkový komentář
+
+/* toto je
+víceřádkový
+komentář
+ */
+
+
+
+
+
+
+
+
+
+
+
