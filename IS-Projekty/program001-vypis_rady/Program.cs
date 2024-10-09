aaa88644c386017039d40c53ae469e86a7bf7524
Tomáš Žižka <tomas.zizka@tul.cz>
2024-10-09 09:42:55 +0200

    #1 výpis řady - základ programu s opakováním

--- IS-Projekty/program001-vypis_rady/Program.cs
+++ IS-Projekty/program001-vypis_rady/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program {
+    static void Main() {
+        
+        string again = "a";
+        
+        while(again == "a") {
+            Console.Clear();
+            Console.WriteLine("****************************");
+            Console.WriteLine("***** Výpis řady čísel *****");
+            Console.WriteLine("****************************");
+            Console.WriteLine("******* Tomáš Žižka ********");
+            Console.WriteLine("****************************");
+            Console.WriteLine();
+
+
+
+            Console.WriteLine();
+            Console.WriteLine("Pro opakování programu stiskněte klávesu A");
+            again = Console.ReadLine();
+
+        }
+
+    }
+}    
+
+
+    
+
+
+
+
+// toto je jednořádkový komentář
+
+/* toto je
+víceřádkový
+komentář
+ */
+
+
+
+
+
+
+
+
+
+
+
