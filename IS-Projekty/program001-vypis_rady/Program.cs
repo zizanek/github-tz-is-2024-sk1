ee2ab54416c9a99873a094690e12b66d7639f527
Tomáš Žižka <tomas.zizka@tul.cz>
2024-10-09 10:22:17 +0200

    #1 výpis řady - DONE

--- IS-Projekty/program001-vypis_rady/Program.cs
+++ IS-Projekty/program001-vypis_rady/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 
 class Program {
     static void Main() {
@@ -14,8 +15,47 @@ class Program {
             Console.WriteLine("****************************");
             Console.WriteLine();
 
+            // Vstup hodnot do programu - špatně řešený
+            // Console.Write("Zadejte první číslo řady: ");
+            // int first = int.Parse(Console.ReadLine());
 
+            // Vstup hodnot do programu - správně řešený
+            Console.Write("Zadejte první číslo řady (celé číslo): ");
+            int first;
+            while(!int.TryParse(Console.ReadLine(), out first)) {
+                Console.Write("Nezadali jste celé číslo. Zadejte první číslo řady znovu: ");
+            }
 
+            Console.Write("Zadejte poslední číslo řady (celé číslo): ");
+            int last;
+            while(!int.TryParse(Console.ReadLine(), out last)) {
+                Console.Write("Nezadali jste celé číslo. Zadejte poslední číslo řady znovu: ");
+            }
+
+            Console.Write("Zadejte diferenci (celé číslo): ");
+            int step;
+            while(!int.TryParse(Console.ReadLine(), out step)) {
+                Console.Write("Nezadali jste celé číslo. Zadejte diference znovu: ");
+            }
+
+            // Výpis zadaných hodnot
+            Console.WriteLine();
+            Console.WriteLine("Zadali jste tyto hodnoty: ");
+            Console.WriteLine("První číslo řady: {0}", first);
+            Console.WriteLine("Poslední číslo řady: {0}", last);
+            Console.WriteLine("Diference: {0}", step);
+
+            // Výpis řady
+            Console.WriteLine();
+            Console.WriteLine("==================");
+            Console.WriteLine("Výpis číselné řady");
+            int current = first;
+            while(current <= last) {
+                Console.WriteLine(current);
+                current = current + step; // Ruční přičtení diference
+            }    
+
+            Console.WriteLine();
             Console.WriteLine();
             Console.WriteLine("Pro opakování programu stiskněte klávesu A");
             again = Console.ReadLine();
