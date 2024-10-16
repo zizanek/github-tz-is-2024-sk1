b6826a08ba8c80f92ef855884488e17741b889b2
Tomáš Žižka <tomas.zizka@tul.cz>
2024-10-16 10:00:26 +0200

    #2 součet a součin cifer

---
+++ IS-Projekty/program002a-soucet-cifer/Program.cs
@@ -0,0 +1,48 @@
+﻿string again = "a";
+        
+        while(again == "a") {
+            Console.Clear();
+            Console.WriteLine("***********************************");
+            Console.WriteLine("***** Ciferný součet a součin *****");
+            Console.WriteLine("***********************************");
+            Console.WriteLine("********** Tomáš Žižka ************");
+            Console.WriteLine("***********************************");
+            Console.WriteLine();
+
+            Console.Write("Zadejte celé číslo: ");
+            int number;
+            while(!int.TryParse(Console.ReadLine(), out number)) {
+                Console.Write("Nezadali jste celé číslo. Zadejte znovu: ");
+            }
+
+            int suma = 0;
+            int multi = 1;
+            int numberBackup = number;
+            int digit;
+
+            if(number<0)
+                number = - number;
+
+            while(number >= 10) {
+                digit = number % 10;
+                number = (number-digit)/10;
+                Console.WriteLine("Digit = {0}", digit);
+                suma = suma + digit;
+                multi = multi * digit;
+            }
+            Console.WriteLine("Digit = {0}", number);
+
+            // musíme přičíst ještě poslední cifru
+            suma = suma + number;
+
+            // musíme ještě donásobit poslední cifru
+            multi = multi * number;
+            
+            Console.WriteLine();
+            Console.WriteLine("\n\n\nSoučet cifer čísla {0} je {1}", numberBackup, suma);
+            Console.WriteLine("\nSoučin cifer čísla {0} je {1}", numberBackup, multi);
+          
+            Console.WriteLine();
+            Console.WriteLine("Pro opakování programu stiskněte klávesu A");
+            again = Console.ReadLine();
+        }
