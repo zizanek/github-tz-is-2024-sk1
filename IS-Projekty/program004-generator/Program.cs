7622e230eacf2553e240fa5971c24d7ef7068fd7
Tomáš Žižka <tomas.zizka@tul.cz>
2024-10-23 10:06:26 +0200

    #4 Generátor - kladna, zaporna, nuly

--- IS-Projekty/program004-generator/Program.cs
+++ IS-Projekty/program004-generator/Program.cs
@@ -41,12 +41,37 @@ string again = "a";
 
             Random randomNumber = new Random();
 
+            int kladna = 0;
+            int zaporna = 0;
+            int nuly = 0;
+
             Console.WriteLine("\n\nNáhodná čísla:");
             for(int i=0; i<n; i++) {
                 myArray[i] = randomNumber.Next(dm, hm+1);
                 Console.Write("{0}; ", myArray[i]); 
+
+                // if(myArray[i]>0)
+                //     kladna++;
+                // if(myArray[i]<0)
+                //     zaporna++;
+                // if(myArray[i]==0)
+                //     nuly++;
+
+                if(myArray[i]>0)
+                    kladna++;
+                else if(myArray[i]<0)
+                    zaporna++;
+                else
+                    nuly++;              
+
             }
-            
+
+
+            Console.WriteLine();
+            Console.WriteLine("\n\n\nPočet kladných čísel: {0}", kladna);    
+            Console.WriteLine("Počet záporných čísel: {0}", zaporna);    
+            Console.WriteLine("Počet nul: {0}", nuly);    
+
             Console.WriteLine();
             Console.WriteLine("Pro opakování programu stiskněte klávesu A");
             again = Console.ReadLine();
