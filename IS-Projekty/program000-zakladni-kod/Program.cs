51605481b1cbd1031c82c9bf8076199161d6d24c
zizanek <tomas.zizka@tul.cz>
2024-11-27 08:11:41 +0100

    uprava zakladniho kodu

--- IS-Projekty/program000-zakladni-kod/Program.cs
+++ IS-Projekty/program000-zakladni-kod/Program.cs
@@ -1,50 +1,28 @@
-﻿using System;
+﻿class Program {
 
-class Program {
     static void Main() {
-        
+
+        // chci, aby se program opakoval po stisku klávesy "a"
         string again = "a";
-        
         while(again == "a") {
-            Console.Clear();
-            Console.WriteLine("****************************");
-            Console.WriteLine("***** Výpis řady čísel *****");
-            Console.WriteLine("****************************");
-            Console.WriteLine("******* Tomáš Žižka ********");
-            Console.WriteLine("****************************");
+            //Console.Clear();
+            Console.WriteLine("******************************");
+            Console.WriteLine("******* Název programu *******");
+            Console.WriteLine("******************************");
+            Console.WriteLine("******** Tomáš Žižka *********");
+            Console.WriteLine("******************************\n\n");
             Console.WriteLine();
 
+            Console.Write("Zadejte první číslo řady (celé číslo): ");
+            int first;
+            while(!int.TryParse(Console.ReadLine(), out first)) {
+                Console.Write("Nezadali jste celé číslo. Zadejte znovu první číslo řady (celé číslo): ");
+            }
 
-
-            Console.WriteLine();
-            Console.WriteLine("Pro opakování programu stiskněte klávesu A");
+            
+            
+            Console.WriteLine("Pro opakování programu stiskněte klávesu a");
             again = Console.ReadLine();
-
         }
-
-    }
-}    
-
-
-    
-
-
-
-
-// toto je jednořádkový komentář
-
-/* toto je
-víceřádkový
-komentář
- */
-
-
-
-
-
-
-
-
-
-
-
+    }   
+}
